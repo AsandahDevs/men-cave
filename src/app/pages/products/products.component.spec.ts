a4cc00eec0ed9e9da7f4a7dc43352426adfd1430
Asanda <noelmajola@gmail.com>
2023-07-13 23:35:46 +0200

    test : add two new test cases

--- src/app/pages/products/products.component.spec.ts
+++ src/app/pages/products/products.component.spec.ts
@@ -3,10 +3,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductsComponent } from './products.component';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ProductsDataService } from 'src/app/services/products-data.service';
+import { of } from 'rxjs';
+import { Product } from 'src/app/interfaces/product';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productService: ProductsDataService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,12 +18,30 @@ describe('ProductsComponent', () => {
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ProductsComponent);
+    fixture = TestBed.createComponent(ProductsComponent)
+    productService = TestBed.inject(ProductsDataService);;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should retrieve products on initialization', () => {
+    const mockProducts:Product[] = [{id:1,title:'Gucci jacket',category:'mens clothing',description:'Limited edition Gucci jacket',price:399,image:'https:fakestoreapi/image/7'},{id:2,title:'sweatpants',category:'mens clothing',description:'jogging sweatpants for the gym enthusiasts',price:40.99,image:'https:fakestoreapi/image/9'}];
+    spyOn(productService, 'getProducts').and.returnValue(of(mockProducts));
+
+    component.ngOnInit();
+
+    expect(productService.getProducts).toHaveBeenCalled();
+  });
+
+  it('should unsubscribe from the subscription on component destruction', () => {
+    spyOn(component.menswear$, 'unsubscribe');
+
+    component.ngOnDestroy();
+
+    expect(component.menswear$.unsubscribe).toHaveBeenCalled();
   });
 });
