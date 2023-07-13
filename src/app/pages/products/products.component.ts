45ca69e0d9669760492aac962cffd116d783ee26
Asanda <noelmajola@gmail.com>
2023-07-13 22:02:00 +0200

    update : add functionalty to fetch data

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { Product } from 'src/app/interfaces/product';
+
+import { ProductsDataService } from 'src/app/services/products-data.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
+  products: Product[] = [];
+  menswear$!: Subscription
+  constructor(private productService: ProductsDataService) {}
+
+  ngOnInit() {
+    this.menswear$ = this.productService
+      .getProducts()
+      .subscribe({
+        next: (data:any) => {
+          this.products = data
+          console.log(this.products)
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+      } );
+  }
+
+  ngOnDestroy() {
+    this.menswear$.unsubscribe()
+  }
 
 }
