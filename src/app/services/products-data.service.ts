edb65bcb171095570392aec3caf43ce2d1878ae8
Asanda <noelmajola@gmail.com>
2023-07-23 22:25:47 +0200

    update : replace space in url

--- src/app/services/products-data.service.ts
+++ src/app/services/products-data.service.ts
@@ -10,6 +10,6 @@ export class ProductsDataService  {
   constructor( private http: HttpClient) { }
 
   getProducts() {
-    return this.http.get("https://fakestoreapi.com/products/category/men's clothing")
+    return this.http.get("https://fakestoreapi.com/products/category/men's%20clothing".replace(/ /g, '%20'))
   }
 }
