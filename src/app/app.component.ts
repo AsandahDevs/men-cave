b485b62218a16524ac36b65a1ed8f16a1cb465b9
Asanda <noelmajola@gmail.com>
2023-09-25 21:04:11 +0200

    update : show/hide navbar based on route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from './services/cart.service';
+import { Location } from '@angular/common';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'men-cave';
   imageURL: string = '/../assets/images/manCave.png'
+  currentRoute: string = '';
 
-  constructor(public cartService:CartService){}
+  constructor(private cartService:CartService,private location:Location ){}
+
+  ngOnInit() {
+    this.currentRoute = this.location.path()
+  }
 
   totalCartProducts(){
     return this.cartService.getTotalProducts()
