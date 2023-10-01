b08c51f175291e3d0a4be2621591f7c56bac2179
Asanda <noelmajola@gmail.com>
2023-10-01 23:14:10 +0200

    test: update unit test configuration

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +12,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports:[MdbTabsModule,MdbFormsModule,ReactiveFormsModule,BrowserAnimationsModule]
     })
     .compileComponents();
 
