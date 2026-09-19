6e5f9609d32dc95a98db499f0b10da5315becc7b
Asanda Majola <noelmajola@gmail.com>
2026-09-19 23:56:07 +0200

    feat: add HttpClientTestingModule to NonExistantComponent and ShoppingCartComponent tests

--- src/app/pages/shopping-cart/shopping-cart.component.spec.ts
+++ src/app/pages/shopping-cart/shopping-cart.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShoppingCartComponent } from './shopping-cart.component';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ShoppingCartComponent', () => {
   let component: ShoppingCartComponent;
@@ -10,7 +11,7 @@ describe('ShoppingCartComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ShoppingCartComponent ],
-      imports:[ComponentsModule]
+      imports:[ComponentsModule, HttpClientTestingModule]
     })
     .compileComponents();
 
